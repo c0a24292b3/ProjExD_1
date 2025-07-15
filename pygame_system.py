5c3a0874909ffa0f375907214245a1dc64f2cd42
waki <c0a24292b3@edu.teu.ac.jp>
2025-07-15 15:47:07 +0900

    FPSの変更

--- pygame_system.py
+++ pygame_system.py
@@ -6,21 +6,27 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
 def main():
-    font_lst = [f for f in pg.font.get_fonts() if f != ""]
-    print(font_lst)
-
+    pg.display.set_caption("はじめてのPygame")
     screen = pg.display.set_mode((800, 600))
     clock = pg.time.Clock()
-    font = pg.font.SysFont("hgp創英角ﾎﾟｯﾌﾟ体", 30)
+    font = pg.font.Font(None, 80)
+
+    enn = pg.Surface((20, 20))
+    pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+    enn.set_colorkey((0, 0, 0))
+
+    tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
         
-        txt = font.render("こんにちは世界", True, (255, 255, 255))
-        screen.fill((0, 0, 0))
+        txt = font.render(str(tmr), True, (255, 255, 255))
+        screen.fill((50, 50, 50))
         screen.blit(txt, [300, 200])
+        screen.blit(enn, [100, 400])
         pg.display.update()
-        clock.tick(10)
+        tmr += 1        
+        clock.tick(0.1)
 
 
 if __name__ == "__main__":
